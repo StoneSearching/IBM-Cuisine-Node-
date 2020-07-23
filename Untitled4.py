96a22b4194e5345935c30c434b6672a951034a5a
Natalie <68333668+StoneSearching@users.noreply.github.com>
2020-07-23 15:36:19 -0400

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+pd.set_option("display.max_columns", None)
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+import re
+
+
+# In[5]:
+
+
+import random
+
+
+# In[6]:
+
+
+recipes = pd.read_csv("https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DS0103EN/labs/data/recipes.csv")
+
+print("Data read into dataframe!")
+
+
+# In[7]:
+
+
+column_names = recipes.columns.values
+column_names[0] = "cuisine"
+recipes.columns = column_names
+
+
+# In[8]:
+
+
+recipes["cuisine"] = recipes["cuisine"].str.lower()
+
+
+# In[9]:
+
+
+recipes.loc[recipes["cuisine"] == "austria", "cuisine"] = "austrian"
+recipes.loc[recipes["cuisine"] == "belgium", "cuisine"] = "belgian"
+recipes.loc[recipes["cuisine"] == "china", "cuisine"] = "chinese"
+recipes.loc[recipes["cuisine"] == "canada", "cuisine"] = "canadian"
+recipes.loc[recipes["cuisine"] == "netherlands", "cuisine"] = "dutch"
+recipes.loc[recipes["cuisine"] == "france", "cuisine"] = "french"
+recipes.loc[recipes["cuisine"] == "germany", "cuisine"] = "german"
+recipes.loc[recipes["cuisine"] == "india", "cuisine"] = "indian"
+recipes.loc[recipes["cuisine"] == "indonesia", "cuisine"] = "indonesian"
+recipes.loc[recipes["cuisine"] == "iran", "cuisine"] = "iranian"
+recipes.loc[recipes["cuisine"] == "italy", "cuisine"] = "italian"
+recipes.loc[recipes["cuisine"] == "japan", "cuisine"] = "japanese"
+recipes.loc[recipes["cuisine"] == "israel", "cuisine"] = "jewish"
+recipes.loc[recipes["cuisine"] == "korea", "cuisine"] = "korean"
+recipes.loc[recipes["cuisine"] == "lebanon", "cuisine"] = "lebanese"
+recipes.loc[recipes["cuisine"] == "malaysia", "cuisine"] = "malaysian"
+recipes.loc[recipes["cuisine"] == "mexico", "cuisine"] = "mexican"
+recipes.loc[recipes["cuisine"] == "pakistan", "cuisine"] = "pakistani"
+recipes.loc[recipes["cuisine"] == "philippines", "cuisine"] = "philippine"
+recipes.loc[recipes["cuisine"] == "scandinavia", "cuisine"] = "scandinavian"
+recipes.loc[recipes["cuisine"] == "spain", "cuisine"] = "spanish_portuguese"
+recipes.loc[recipes["cuisine"] == "portugal", "cuisine"] = "spanish_portuguese"
+recipes.loc[recipes["cuisine"] == "switzerland", "cuisine"] = "swiss"
+recipes.loc[recipes["cuisine"] == "thailand", "cuisine"] = "thai"
+recipes.loc[recipes["cuisine"] == "turkey", "cuisine"] = "turkish"
+recipes.loc[recipes["cuisine"] == "vietnam", "cuisine"] = "vietnamese"
+recipes.loc[recipes["cuisine"] == "uk-and-ireland", "cuisine"] = "uk-and-irish"
+recipes.loc[recipes["cuisine"] == "irish", "cuisine"] = "uk-and-irish"
+
+
+# In[10]:
+
+
+recipes_counts = recipes["cuisine"].value_counts()
+cuisines_indices = recipes_counts > 50
+
+
+# In[11]:
+
+
+cuisines_to_keep = list(np.array(recipes_counts.index.values)[np.array(cuisines_indices)])
+recipes = recipes.loc[recipes["cuisine"].isin(cuisines_to_keep)]
+
+
+# In[12]:
+
+
+recipes = recipes.replace(to_replace="Yes", value=1)
+recipes = recipes.replace(to_replace="No", value=0)
+
+
+# In[13]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn import tree
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# In[14]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn import tree
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib.pyplot as plt
+
+get_ipython().system('conda install python-graphviz --yes')
+import graphviz
+
+from sklearn.tree import export_graphviz
+
+import itertools
+
+
+# In[15]:
+
+
+recipes.head()
+
+
+# In[16]:
+
+
+asian_indian_recipes = recipes[recipes.cuisine.isin(["korean", "japanese", "chinese", "thai", "indian"])]
+cuisines = asian_indian_recipes["cuisine"]
+ingredients = asian_indian_recipes.iloc[:,1:]
+
+bamboo_tree = tree.DecisionTreeClassifier(max_depth=3)
+bamboo_tree.fit(ingredients, cuisines)
+
+print("Decision tree model saved to bamboo_tree!")
+
+
+# In[17]:
+
+
+export_graphviz(bamboo_tree,
+                feature_names=list(ingredients.columns.values),
+                out_file="bamboo_tree.dot",
+                class_names=np.unique(cuisines),
+                filled=True,
+                node_ids=True,
+                special_characters=True,
+                impurity=False,
+                label="all",
+                leaves_parallel=False)
+
+with open("bamboo_tree.dot") as bamboo_tree_image:
+    bamboo_tree_graph = bamboo_tree_image.read()
+graphviz.Source(bamboo_tree_graph)
+
+
+# In[18]:
+
+
+bamboo = recipes[recipes.cuisine.isin(["korean", "japanese", "chinese", "thai", "indian"])]
+
+
+# In[19]:
+
+
+bamboo["cuisine"].value_counts()
+
+
+# In[20]:
+
+
+sample_n = 30
+
+
+# In[21]:
+
+
+random.seed(1234) # set random seed
+bamboo_test = bamboo.groupby("cuisine", group_keys=False).apply(lambda x: x.sample(sample_n))
+
+bamboo_test_ingredients = bamboo_test.iloc[:,1:] # ingredients
+bamboo_test_cuisines = bamboo_test["cuisine"] # corresponding cuisines or labels
+
+
+# In[22]:
+
+
+bamboo_test["cuisine"].value_counts()
+
+
+# In[23]:
+
+
+bamboo_test_index = bamboo.index.isin(bamboo_test.index)
+bamboo_train = bamboo[~bamboo_test_index]
+
+bamboo_train_ingredients = bamboo_train.iloc[:,1:]
+bamboo_train_cuisines = bamboo_train["cuisine"]
+
+
+# In[24]:
+
+
+bamboo_train["cuisine"].value_counts()
+
+
+# In[25]:
+
+
+bamboo_train_tree = tree.DecisionTreeClassifier(max_depth=15)
+bamboo_train_tree.fit(bamboo_train_ingredients, bamboo_train_cuisines)
+
+print("Decision tree model saved to bamboo_train_tree!")
+
+
+# In[26]:
+
+
+export_graphviz(bamboo_train_tree,
+                feature_names=list(bamboo_train_ingredients.columns.values),
+                out_file="bamboo_train_tree.dot",
+                class_names=np.unique(bamboo_train_cuisines),
+                filled=True,
+                node_ids=True,
+                special_characters=True,
+                impurity=False,
+                label="all",
+                leaves_parallel=False)
+
+with open("bamboo_train_tree.dot") as bamboo_train_tree_image:
+    bamboo_train_tree_graph = bamboo_train_tree_image.read()
+graphviz.Source(bamboo_train_tree_graph)
+
+
+# In[ ]:
+
+
+
+
+
+# In[27]:
+
+
+bamboo_pred_cuisines = bamboo_train_tree.predict(bamboo_test_ingredients)
+
+
+# In[28]:
+
+
+test_cuisines = np.unique(bamboo_test_cuisines)
+bamboo_confusion_matrix = confusion_matrix(bamboo_test_cuisines, bamboo_pred_cuisines, test_cuisines)
+title = 'Bamboo Confusion Matrix'
+cmap = plt.cm.Blues
+
+plt.figure(figsize=(8, 6))
+bamboo_confusion_matrix = (
+    bamboo_confusion_matrix.astype('float') / bamboo_confusion_matrix.sum(axis=1)[:, np.newaxis]
+    ) * 100
+
+plt.imshow(bamboo_confusion_matrix, interpolation='nearest', cmap=cmap)
+plt.title(title)
+plt.colorbar()
+tick_marks = np.arange(len(test_cuisines))
+plt.xticks(tick_marks, test_cuisines)
+plt.yticks(tick_marks, test_cuisines)
+
+fmt = '.2f'
+thresh = bamboo_confusion_matrix.max() / 2.
+for i, j in itertools.product(range(bamboo_confusion_matrix.shape[0]), range(bamboo_confusion_matrix.shape[1])):
+    plt.text(j, i, format(bamboo_confusion_matrix[i, j], fmt),
+             horizontalalignment="center",
+             color="white" if bamboo_confusion_matrix[i, j] > thresh else "black")
+
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
